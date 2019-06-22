d9b122f93a8e0e0bf6043ea8ce6819efcfb0bb00
Emily Lin <el3015@nyu.edu>
2019-06-22 15:36:53 -0400

    js + css updates

--- 9-10_code/pearl.js
+++ 9-10_code/pearl.js
@@ -1,49 +1,35 @@
 //for pearl
 
 function setup() {
-  createCanvas(710, 400, WEBGL);
+  let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.position(0,0);
+  canvas.style('z-index', '1');
+
+   // Move the canvas so it’s inside our <div id="sketch-holder">.
+   canvas.parent('sketch-holder');
 }
 
 function draw() {
-  background(255,205,205);
+  clear();
 
-  let locX = mouseX/2;
-  let locY = mouseY/2;
+  let pointOneX = mouseX+200;
+  let pointOneY = mouseY+200;
 
-  ambientLight(190);
-  directionalLight(255, 0, 0, 20, 0, -1);
-  pointLight(255, 0, 255, locX, locY, 400);
+  let pointTwoX = mouseX - width/2;
+  let pointTwoY = mouseY - height/2;
 
 
-  ambientMaterial(255,255,0);
-  fill(255,255,255);
-  sphere(100, 640);
+  ambientLight(90);
+  directionalLight(255, 150, 100, -1, -2, -1);
+  directionalLight(200,150,220, 0, 1, -1);
+  //directionalLight(0,10,0, 0, 0, -1);
+  // pointLight(255, 0, 229, 50, -1, 0, 100);
+  pointLight(255, 210, 229, pointTwoX, pointTwoY, 200);
+
+
+  specularMaterial(255,235,248);
+  //fill(255,200,0);
+  sphere(220, 900);
+  // rotateY(millis() / 1000);
 }
 
-
-
-//for gradient
-var c1, c2;
-
-function setup() {
-  createCanvas(600, 600);
-  // Define colors
-  c1 = color(255, 204, 0);
-  c2 = color(255);
-  setGradient(c1, c2);
-}
-
-function draw() {
-  //ellipse(mouseX, mouseY, 30, 30);
-}
-
-function setGradient(c1, c2) {
-  // noprotect
-  noFill();
-  for (var y = 0; y < height; y++) {
-    var inter = map(y, 0, height, 0, 1);
-    var c = lerpColor(c1, c2, inter);
-    stroke(c);
-    line(0, y, width, y);
-  }
-}
