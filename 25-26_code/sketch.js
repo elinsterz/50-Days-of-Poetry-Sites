084db566f01f97666592d58ea34f922f79cf780c
emilylin-itp.github.io <el3015@nyu.edu>
2019-08-02 23:41:24 -0400

    day 25-26

---
+++ 25-26_code/sketch.js
@@ -0,0 +1,86 @@
+let font;
+let vehicles = [];
+
+// let bounds;
+let splitString;
+let myString;
+let first;
+let second;
+let third;
+let fourth;
+
+function preload() {
+  font = loadFont('style/fonts/apercu_bold.otf');
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  //split sentence into smaller strings
+  let myString = 'HER,BIRTHDAY AS,ASHES IN,SEAWATER'
+  let splitString = split(myString, ',');
+
+  first = splitString[0];
+  second = splitString[1];
+  third = splitString[2];
+  fourth = splitString[3];
+
+  //Create variables for each split string
+  let first_points = font.textToPoints(first, 100, 300, 224, {
+    sampleFactor: 0.25
+  });
+
+  let second_points = font.textToPoints(second, 100, 500, 224, {
+    sampleFactor: 0.25
+  });
+
+  let third_points = font.textToPoints(third, 100, 700, 224, {
+    sampleFactor: 0.25
+  });
+
+  let fourth_points = font.textToPoints(fourth, 100, 900, 224, {
+    sampleFactor: 0.25
+  });
+
+
+  //drawing each string 
+  for (let i = 0; i < first_points.length; i++) {
+    let pt = first_points[i];
+    let vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+  }
+
+  for (let i = 0; i < second_points.length; i++) {
+    let pt = second_points[i];
+    let vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+  }
+
+  for (let i = 0; i < third_points.length; i++) {
+    let pt = third_points[i];
+    let vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+  }
+ 
+    for (let i = 0; i < fourth_points.length; i++) {
+    let pt = fourth_points[i];
+    let vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+  }
+  
+}
+
+
+function draw() {
+
+    background(35,177,210);
+
+  for (let i = 0; i < vehicles.length; i++) {
+    let v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+  }
+
+}
---
+++ 25-26_code/vehicles.js
@@ -0,0 +1,66 @@
+function Vehicle(x, y) {
+    this.pos = createVector(random(width), random(height));
+    this.target = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.acc = createVector();
+    this.r = 1.5;
+    this.maxspeed = 8;
+    this.maxforce = 22;
+  }
+  
+  Vehicle.prototype.behaviors = function() {
+    let arrive = this.arrive(this.target);
+    let mouse = createVector(mouseX, mouseY);
+    let flee = this.flee(mouse);
+  
+    arrive.mult(1);
+    flee.mult(10);
+  
+    this.applyForce(arrive);
+    this.applyForce(flee);
+  }
+  
+  Vehicle.prototype.applyForce = function(f) {
+    this.acc.add(f);
+  }
+  
+  Vehicle.prototype.update = function() {
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.mult(0);
+  }
+  
+  Vehicle.prototype.show = function() {
+    stroke(255);
+    strokeWeight(this.r);
+    point(this.pos.x, this.pos.y);
+  }
+  
+  
+  Vehicle.prototype.arrive = function(target) {
+    let desired = p5.Vector.sub(target, this.pos);
+    let d = desired.mag();
+    let speed = this.maxspeed;
+    if (d < 50) {
+      speed = map(d, 0, 100, 0, this.maxspeed);
+    }
+    desired.setMag(speed);
+    let steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxforce);
+    return steer;
+  }
+  
+  Vehicle.prototype.flee = function(target) {
+    let desired = p5.Vector.sub(target, this.pos);
+    let d = desired.mag();
+    if (d < 50) {
+      desired.setMag(this.maxspeed);
+      desired.mult(-1);
+      var steer = p5.Vector.sub(desired, this.vel);
+      steer.limit(this.maxforce);
+      return steer;
+    } else {
+      return createVector(0, 0);
+    }
+  }
+  
