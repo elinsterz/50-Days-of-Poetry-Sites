2de4819fe49d6eccdfba30c48b37fd04867884e2
emilylin-itp.github.io <el3015@nyu.edu>
2019-08-21 00:34:40 -0400

    update

--- 35-36_code/main.js
+++ 35-36_code/main.js
@@ -1,13 +1,116 @@
-function openFunction() {
+function openPoem(poemName,itemNum) {
 
-    let poem = document.getElementById('poem-text');
-    let item1 = document.getElementById('item1')
+    let poem = document.getElementById(poemName);
+    let item = document.getElementById(itemNum);
 
     if (poem.style.display !== 'none') {
         poem.style.display = 'none';
-        item1.style.backgroundColor = 'yellow';
+        item.style.backgroundColor = 'yellow';
     } else {
         poem.style.display = 'block';
-        item1.style.backgroundColor = 'white';
+        item.style.backgroundColor  = 'white';
     }
 }
+
+
+// function openPoemOne() {
+
+//     let poem = document.getElementById('poem1');
+//     let item = document.getElementById('item1');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+
+// function openPoemTwo() {
+
+//     let poem = document.getElementById('poem2');
+//     let item = document.getElementById('item2');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+// function openPoemThree() {
+
+//     let poem = document.getElementById('poem3');
+//     let item = document.getElementById('item3');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+// function openPoemFour() {
+
+//     let poem = document.getElementById('poem4');
+//     let item = document.getElementById('item4');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+// function openPoemFive() {
+
+//     let poem = document.getElementById('poem5');
+//     let item = document.getElementById('item5');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+// function openPoemSix() {
+
+//     let poem = document.getElementById('poem6');
+//     let item = document.getElementById('item6');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+// function openPoemSeven() {
+
+//     let poem = document.getElementById('poem7');
+//     let item = document.getElementById('item7');
+
+//     if (poem.style.display !== 'none') {
+//         poem.style.display = 'none';
+//         item.style.backgroundColor = 'yellow';
+//     } else {
+//         poem.style.display = 'block';
+//         item.style.backgroundColor  = 'white';
+//     }
+// }
+
+
+
--- 35-36_code/sketch.js
+++ 35-36_code/sketch.js
@@ -8,7 +8,7 @@ function preload() {
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
-    canvas.style('z-index', '1');
+    canvas.style('z-index', '-1');
 
     // Move the canvas so it’s inside our <div id="sketch-holder">.
     canvas.parent('sketch-holder');
@@ -26,5 +26,5 @@ function draw() {
     translate(width/2, height/2);
     rotate(x);
     // rect(0, 0, 100, 100);
-    image(img, 0, 0, 1024, 1024);
+    image(img, 0, 0, 960, 960);
 }
