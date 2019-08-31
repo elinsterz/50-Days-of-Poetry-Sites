0888fc1fd7cda80e5d6b3bd13dad14e746400bbf
emilylin-itp.github.io <el3015@nyu.edu>
2019-08-30 23:39:26 -0400

    update sketch

--- 41-42_code/sketch.js
+++ 41-42_code/sketch.js
@@ -1,5 +1,11 @@
-let phrase = "Kindness";
-let size = 180;
+let phraseA = "The Kindness";
+let phraseB = "of Others";
+let phraseA_x;
+let phraseA_y;
+let phraseB_x;
+let phraseB_y;
+
+let size = 160;
 let points;
 let font;
 let randomColorR;
@@ -17,18 +23,25 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   fill(0);
+  
+  //background colors
   randomColorR= random(225,255);
   randomColorG= random(150,220);
   randomColorB= random(150,220);
   
-  x = windowWidth/2 - 420;
-  y = windowHeight/2 + 50;
+  // text placement
+  phraseA_x = windowWidth/2-250;
+  phraseA_y = windowHeight/2;
+  phraseB_x = windowWidth/2-250;
+  phraseB_y = windowHeight/2 +150;
+  
   options = {
-    	sampleFactor: 1,
+    	sampleFactor: 0.5,
     	simplifyThreshold: 0
   };
   
-  points = font.textToPoints(phrase, x, y, size, options);
+  points = font.textToPoints(phraseA, phraseA_x, phraseA_y, size, options);
+  points = font.textToPoints(phraseB, phraseB_x, phraseB_y, size, options);
   
   background(randomColorR, randomColorG, randomColorB);
   textSize(size);
@@ -39,15 +52,16 @@ function draw() {
   
   
   fill(randomColorR, randomColorG, randomColorB,50);
-  text(phrase, x, y);
+  text(phraseA, phraseA_x, phraseA_y);
+  text(phraseB, phraseB_x, phraseB_y);
   
    fill(255,3);
   
   for(let i = 0; i< points.length-1; i++){
    var pt = points[i];
     
-    pt.x += noise(i/5, frameCount * 0.005) * 2.0 - 1.0;
-    pt.y += noise(i/4, frameCount * 0.005) * 2.0 - 1.0;
+    pt.x += noise(i/25, frameCount * 0.005) * 2.0 - 1.0;
+    pt.y += noise(i/55, frameCount * 0.005) * 2.0 - 1.0;
     
     fill(noise(i*0.02, frameCount*0.015) + 255,random(0,100));
     ellipse(pt.x, pt.y,1);
